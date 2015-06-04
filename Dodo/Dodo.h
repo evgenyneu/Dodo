b9762cc12008077cb0c01b845623c31302cfcf0e
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-05 08:17:25 +1000

    Initial Commit

---
+++ Dodo/Dodo.h
@@ -0,0 +1,19 @@
+//
+//  Dodo.h
+//  Dodo
+//
+//  Created by Evgenii on 5/06/2015.
+//  Copyright (c) 2015 Marketplacer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dodo.
+FOUNDATION_EXPORT double DodoVersionNumber;
+
+//! Project version string for Dodo.
+FOUNDATION_EXPORT const unsigned char DodoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dodo/PublicHeader.h>
+
+
