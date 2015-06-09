e582d99da44529d7ed44bcaaf2e14746a6f097f7
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-09 15:58:46 +1000

    Rearrange styles

--- Dodo/Style/DodoBarDefaultStyles.swift
+++ Dodo/Style/DodoBarDefaultStyles.swift
@@ -10,104 +10,37 @@ public struct DodoBarDefaultStyles {
   
   /// Revert the property values to their defaults
   public static func resetToDefaults() {
+    animationHide = _animationHide
+    animationHideDuration = _animationHideDuration
+    animationShow = _animationShow
+    animationShowDuration = _animationShowDuration
+    backgroundColor = _backgroundColor
+    borderColor = _borderColor
+    borderWidth = _borderWidth
+    cornerRadius = _cornerRadius
+    debugMode = _debugMode
     hideAfterDelaySeconds = _hideAfterDelaySeconds
     hideOnTap = _hideOnTap
     locationTop = _locationTop
     marginToSuperview = _marginToSuperview
-    cornerRadius = _cornerRadius
-    borderWidth = _borderWidth
-    borderColor = _borderColor
-    backgroundColor = _backgroundColor
-    debugMode = _debugMode
-    animationShow = _animationShow
-    animationShowDuration = _animationShowDuration
-    animationHide = _animationHide
-    animationHideDuration = _animationHideDuration
   }
   
   // ---------------------------
-
-  
-  private static let _hideAfterDelaySeconds: NSTimeInterval = 0
-  
-  /**
-  
-  Hides the bar automatically after the specified number of seconds.
-  The bar is kept on screen indefinitely if the value is zero.
-  
-  */
-  public static var hideAfterDelaySeconds = _hideAfterDelaySeconds
   
   
-  // ---------------------------
-
+  private static let _animationHide: DodoAnimation = DodoAnimationsHide.rotate
   
-  private static let _hideOnTap = false
-  
-  /// When true it hides the bar when it is tapped
-  public static var hideOnTap = _hideOnTap
-  
-
-  // ---------------------------
-
-  
-  private static let _locationTop = true
-  
-  /// Position of the bar. When true the bar is shown on top of the screen.
-  public static var locationTop = _locationTop
-  
-
-  // ---------------------------
-  
-  
-  private static let _marginToSuperview = CGSize(width: 5, height: 5)
-  
-  /// Margin between the bar edge and its superiew.
-  public static var marginToSuperview = _marginToSuperview
-  
-
-  // ---------------------------
-
-  
-  private static let _cornerRadius: CGFloat = 20
-  
-  /// Corner radius of bar edges.
-  public static var cornerRadius = _cornerRadius
-  
-  
-  // ---------------------------
-
-  
-  private static let _borderWidth: CGFloat  = 1 / UIScreen.mainScreen().scale
-  
-  /// Border width of the bar.
-  public static var borderWidth = _borderWidth
-  
-  
-  // ---------------------------
-
-  
-  private static let _borderColor: UIColor? = nil
-  
-  /// Color of toolbar border.
-  public static var borderColor = _borderColor
+  /// Callback for animating the bar when it is hidden.
+  public static var animationHide: DodoAnimation = _animationHide
   
   
   // ---------------------------
   
   
-  private static let _backgroundColor: UIColor? = nil
+  private static let _animationHideDuration: NSTimeInterval? = nil
   
-  /// Background color of the bar.
-  public static var backgroundColor = _backgroundColor
-  
-  
-  // ---------------------------
-
-  private static let _debugMode = false
-  
-  /// Highlights the view backgrounds for spotting layout issues.
-  public static var debugMode = _debugMode
+  /// Duration of hide animation. When nil it uses default duration for selected animation function.
+  public static var animationHideDuration: NSTimeInterval? = _animationHideDuration
   
   
   // ---------------------------
@@ -131,21 +64,88 @@ public struct DodoBarDefaultStyles {
   // ---------------------------
   
   
-  private static let _animationHide: DodoAnimation = DodoAnimationsHide.rotate
+  private static let _backgroundColor: UIColor? = nil
   
-  /// Callback for animating the bar when it is hidden.
-  public static var animationHide: DodoAnimation = _animationHide
-
+  /// Background color of the bar.
+  public static var backgroundColor = _backgroundColor
+  
+  
+  // ---------------------------
+  
+  
+  private static let _borderColor: UIColor? = nil
+  
+  /// Color of toolbar border.
+  public static var borderColor = _borderColor
+  
   
   // ---------------------------
   
   
-  private static let _animationHideDuration: NSTimeInterval? = nil
+  private static let _borderWidth: CGFloat  = 1 / UIScreen.mainScreen().scale
   
-  /// Duration of hide animation. When nil it uses default duration for selected animation function.
-  public static var animationHideDuration: NSTimeInterval? = _animationHideDuration
+  /// Border width of the bar.
+  public static var borderWidth = _borderWidth
+  
+  
+  // ---------------------------
+  
+  
+  private static let _cornerRadius: CGFloat = 20
+  
+  /// Corner radius of bar edges.
+  public static var cornerRadius = _cornerRadius
+  
+  
+  // ---------------------------
+  
+  
+  private static let _debugMode = false
+  
+  /// Highlights the view backgrounds for spotting layout issues.
+  public static var debugMode = _debugMode
+  
+  
+  // ---------------------------
+  
+  
+  private static let _hideAfterDelaySeconds: NSTimeInterval = 0
+  
+  /**
+  
+  Hides the bar automatically after the specified number of seconds.
+  The bar is kept on screen indefinitely if the value is zero.
+  
+  */
+  public static var hideAfterDelaySeconds = _hideAfterDelaySeconds
+  
+  
+  // ---------------------------
+  
+  
+  private static let _hideOnTap = false
+  
+  /// When true it hides the bar when it is tapped
+  public static var hideOnTap = _hideOnTap
   
   
   // ---------------------------
 
+  
+  private static let _locationTop = true
+  
+  /// Position of the bar. When true the bar is shown on top of the screen.
+  public static var locationTop = _locationTop
+  
+
+  // ---------------------------
+  
+  
+  private static let _marginToSuperview = CGSize(width: 5, height: 5)
+  
+  /// Margin between the bar edge and its superiew.
+  public static var marginToSuperview = _marginToSuperview
+  
+
+  // ---------------------------
 }
